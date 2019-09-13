0e51d0db15e36baf1d5a3e5ff056b9f9d98ab2d3
Kole Nunley <kfnunley@gmail.com>
2019-09-13 09:36:19 -0400

    shorten loop

--- actuator_control_test.py
+++ actuator_control_test.py
@@ -23,7 +23,7 @@ class ActuatorControlTest():
             # self.actuator_control_message.angular_velocities = inputs
             # self.message_pub.publish(self.actuator_control_message)
             os.system("rostopic pub --once /firefly/command/motor_speed mav_msgs/Actuators '{angular_velocities: [0, 0, 0, 1000, 0, 1000]}'")
-            time.sleep(10)
+            time.sleep(0.5)
 
 
 
