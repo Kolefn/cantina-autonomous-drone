ac7a014f766ba5354ab5ed8490edc4ef8b523961
Kole Nunley <kfnunley@gmail.com>
2019-09-13 08:01:33 -0400

    setup script

---
+++ simulation/setup.sh
@@ -0,0 +1,22 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavlink python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox
+sudo rosdep init
+rosdep update
+source /opt/ros/kinetic/setup.bash
+
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace  # initialize your catkin workspace
+wstool init
+wget https://raw.githubusercontent.com/ethz-asl/rotors_simulator/master/rotors_hil.rosinstall
+wstool merge rotors_hil.rosinstall
+wstool update
+
+cd ~/catkin_ws/
+catkin build
+
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
