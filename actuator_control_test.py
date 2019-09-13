e897a33c3f39b1df5ce9647e0f76ec1aef87e58c
Kole Nunley <kfnunley@gmail.com>
2019-09-13 09:35:29 -0400

    loop

--- actuator_control_test.py
+++ actuator_control_test.py
@@ -17,12 +17,13 @@ class ActuatorControlTest():
     def run(self):
 
 
-        inputs = [0, 0, 0, 0, 0, 0]
+        while True: 
+            inputs = [0, 0, 0, 0, 0, 0]
 
-        # self.actuator_control_message.angular_velocities = inputs
-        # self.message_pub.publish(self.actuator_control_message)
-        os.system("rostopic pub --once /firefly/command/motor_speed mav_msgs/Actuators '{angular_velocities: [0, 0, 0, 1000, 0, 1000]}'")
-        time.sleep(10)
+            # self.actuator_control_message.angular_velocities = inputs
+            # self.message_pub.publish(self.actuator_control_message)
+            os.system("rostopic pub --once /firefly/command/motor_speed mav_msgs/Actuators '{angular_velocities: [0, 0, 0, 1000, 0, 1000]}'")
+            time.sleep(10)
 
 
 
