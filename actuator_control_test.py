5d0cace22ed253bdfb2ad099497c8450c870dcd4
Kole Nunley <kfnunley@gmail.com>
2019-09-13 12:09:22 -0400

    up and down

--- actuator_control_test.py
+++ actuator_control_test.py
@@ -13,18 +13,30 @@ class ActuatorControlTest():
 
         self.message_pub = rospy.Publisher("/hummingbird/command/motor_speed", Actuators, queue_size=3)
         self.actuator_control_message = Actuators()
+    
+    def setAngularVelocities(inputs):
+        self.actuator_control_message.angular_velocities = inputs
+        self.message_pub.publish(self.actuator_control_message)
+
+    def up(self):
+        inputs = [500, 500, 500, 500]
+        self.setAngularVelocities(inputs);
+    
+    def down(self):
+        inputs = [100, 100, 100, 100]
+        self.setAngularVelocities(inputs);
 
     def run(self):
 
-            while self.message_pub.get_num_connections() == 0:
-                rospy.loginfo("Waiting for subscriber to connect")
-                rospy.sleep(1)
-            while True:
-                inputs = [500, 500, 500, 500]
-
-                self.actuator_control_message.angular_velocities = inputs
-                self.message_pub.publish(self.actuator_control_message)
-                rospy.sleep(1)
+        while self.message_pub.get_num_connections() == 0:
+            rospy.loginfo("Waiting for subscriber to connect")
+            rospy.sleep(1)
+        
+        while True:
+            self.up()
+            rospy.sleep(2)
+            self.down()
+            rospy.sleep(2)
 
 
 
