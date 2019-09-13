20c527664c0a8e3b5583aee19cae1fde1b5c0a3f
Kole Nunley <kfnunley@gmail.com>
2019-09-13 09:45:51 -0400

    add back loop

--- actuator_control_test.py
+++ actuator_control_test.py
@@ -11,7 +11,7 @@ class ActuatorControlTest():
     def __init__(self,
                  ):
 
-        self.message_pub = rospy.Publisher("/firefly/command/motor_speed", Actuators, queue_size=1)
+        self.message_pub = rospy.Publisher("/firefly/command/motor_speed", Actuators, queue_size=3)
         self.actuator_control_message = Actuators()
 
     def run(self):
@@ -19,12 +19,12 @@ class ActuatorControlTest():
             while self.message_pub.get_num_connections() == 0:
                 rospy.loginfo("Waiting for subscriber to connect")
                 rospy.sleep(1)
+            while True:
+                inputs = [0, 0, 0, 0, 0, 0]
 
-            inputs = [0, 0, 0, 0, 0, 0]
-
-            self.actuator_control_message.angular_velocities = inputs
-            self.message_pub.publish(self.actuator_control_message)
-            # os.system("rostopic pub --once /firefly/command/motor_speed mav_msgs/Actuators '{angular_velocities: [0, 0, 0, 1000, 0, 1000]}'")
+                self.actuator_control_message.angular_velocities = inputs
+                self.message_pub.publish(self.actuator_control_message)
+                rospy.sleep(1)
 
 
 
