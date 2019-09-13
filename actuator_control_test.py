3a2aba944be33a1d3a9910ad40733930e26d1937
Kole Nunley <kfnunley@gmail.com>
2019-09-13 10:13:27 -0400

    actuator numbers

--- actuator_control_test.py
+++ actuator_control_test.py
@@ -20,7 +20,7 @@ class ActuatorControlTest():
                 rospy.loginfo("Waiting for subscriber to connect")
                 rospy.sleep(1)
             while True:
-                inputs = [0, 0, 0, 0, 0, 0]
+                inputs = [500, 500, 500, 0, 0, 0]
 
                 self.actuator_control_message.angular_velocities = inputs
                 self.message_pub.publish(self.actuator_control_message)
