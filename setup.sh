dea556773f2665a59924e7387f11d34bc2bdbfe7
Kole Nunley <kfnunley@gmail.com>
2019-09-24 17:20:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main"
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavlink python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox
-sudo rosdep init
-rosdep update
-source /opt/ros/kinetic/setup.bash
+
 
 
 # upgrade default version of Gazebo that comes with ROS kinetic
@@ -13,6 +11,9 @@ wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install gazebo7 -y
 
+sudo rosdep init
+rosdep update
+source /opt/ros/kinetic/setup.bash
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
