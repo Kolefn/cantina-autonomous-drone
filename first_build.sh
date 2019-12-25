58c49238443dcb899879414b8fed72464d33c263
Kole Nunley <kfnunley@gmail.com>
2019-12-25 12:52:16 -0500

    build script adjustments

--- first_build.sh
+++ first_build.sh
@@ -4,6 +4,6 @@ colcon build
 cd ..
 
 
-echo "source deeprotor_ws/install/local_setup.bash" >> ~/.bashrc
+echo "source $(pwd)/deeprotor_ws/install/local_setup.bash" >> ~/.bashrc
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
