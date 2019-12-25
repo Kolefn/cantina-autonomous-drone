c911f6775edceb38c13cf865e5bcbcb27d836151
Kole Nunley <kfnunley@gmail.com>
2019-12-25 10:21:34 -0500

    Update setup_dev_env.sh

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -1,8 +1,9 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavlink python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox
-
+sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavlink python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox python3-pip python3-apt
+pip3 install -U setuptools
+pip3 install -U colcon-common-extensions colcon-ros-bundle
 
 
 # upgrade default version of Gazebo that comes with ROS kinetic
