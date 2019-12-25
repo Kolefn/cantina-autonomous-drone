b93ff3897d64359cca0efae7e9fc37f6b47dfb63
Kole Nunley <kfnunley@gmail.com>
2019-12-25 12:38:39 -0500

    move rotors_simulator as workspace level package

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,8 +1,8 @@
 # download and setup rotors_simulator dependencies
-wstool init deeprotor_ws/src/deeprotor_simulation_environment
-wget -O deeprotor_ws/src/deeprotor_simulation_environment/rotors_hil.rosinstall https://raw.githubusercontent.com/ethz-asl/rotors_simulator/master/rotors_hil.rosinstall
-wstool merge -t deeprotor_ws/src/deeprotor_simulation_environment deeprotor_ws/src/deeprotor_simulation_environment/rotors_hil.rosinstall
-wstool update -t deeprotor_ws/src/deeprotor_simulation_environment
+wstool init deeprotor_ws/src/
+wget -O deeprotor_ws/src/rotors_hil.rosinstall https://raw.githubusercontent.com/ethz-asl/rotors_simulator/master/rotors_hil.rosinstall
+wstool merge -t deeprotor_ws/src deeprotor_ws/src/rotors_hil.rosinstall
+wstool update -t deeprotor_ws/src
 
 # install additional dependencies for rotors_simulator
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -14,7 +14,7 @@ sudo apt-get install geographiclib-tools
 sudo apt-get install libignition-math2-dev
 
 # fix bug in dependency code
-sed -i 's/msg.twist_covariance/msg.velocity_covariance/g' deeprotor_ws/src/deeprotor_simulation_environment/mavros/mavros_extras/src/plugins/odom.cpp
+sed -i 's/msg.twist_covariance/msg.velocity_covariance/g' deeprotor_ws/src/mavros/mavros_extras/src/plugins/odom.cpp
 
 # Setup for bundling & running
 sudo rosdep init
