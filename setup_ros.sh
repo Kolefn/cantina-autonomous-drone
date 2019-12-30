9711316473c8c8661e17d856144035e0d8fcdd61
Kole Nunley <kfnunley@gmail.com>
2019-12-30 12:56:58 -0500

    Update setup_ros.sh

--- setup_ros.sh
+++ setup_ros.sh
@@ -3,11 +3,14 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main"
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavlink python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox python3-pip python3-apt
-pip3 install -U setuptools
-pip3 install -U colcon-common-extensions colcon-ros-bundle
+
 
 # upgrade default version of Gazebo that comes with ROS kinetic
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install gazebo7 -y
+
+# Colcon for build and bundling 
+pip3 install -U setuptools
+pip3 install -U colcon-common-extensions colcon-ros-bundle
