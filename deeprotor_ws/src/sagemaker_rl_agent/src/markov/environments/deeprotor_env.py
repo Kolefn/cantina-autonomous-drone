9e6e2a169c0c5568fd8ac4a9f99862125f553185
Kole Nunley <kfnunley@gmail.com>
2020-01-10 09:41:38 -0500

    increase Actuator publish que size

--- deeprotor_ws/src/sagemaker_rl_agent/src/markov/environments/deeprotor_env.py
+++ deeprotor_ws/src/sagemaker_rl_agent/src/markov/environments/deeprotor_env.py
@@ -131,7 +131,7 @@ class DeepRotorEnv(gym.Env):
                                                        ResetDroneSrv)
                                         
 
-            self.velocity_pub = rospy.Publisher("/drone/command/motor_speed", Actuators, queue_size=1)
+            self.velocity_pub = rospy.Publisher("/drone/command/motor_speed", Actuators, queue_size=10)
 
             # Read in parameters
             self.job_type = rospy.get_param('JOB_TYPE')
