b2ffab0cf7396ef0a38d01c4336fae31592ff8ea
Kole Nunley <kfnunley@gmail.com>
2020-01-13 15:58:40 -0500

    factor in current yaw and pitch

--- deeprotor_ws/src/sagemaker_rl_agent/src/markov/rewards/default.py
+++ deeprotor_ws/src/sagemaker_rl_agent/src/markov/rewards/default.py
@@ -10,6 +10,9 @@ def reward_function(params):
     y = params['y']
     z = params['z']
 
+    current_pitch = params['pitch']
+    current_yaw = params['yaw']
+
     dx = target_x - x 
     dy = target_y - y
     dz = target_z - z
@@ -17,16 +20,16 @@ def reward_function(params):
     xymag = math.sqrt(dx*dx + dy*dy)
 
     # rotations needed for drone camera to face target
-    pitch = math.atan2(dz, xymag)
+    pitch = math.atan2(dz, xymag) - current_pitch
 
-    starting_offset = math.atan2(-1.5, 1.5) #@TODO dont hardcode the targets x,y here
-    yaw = math.atan2(dy, dx) - starting_offset
+    starting_yaw_offset = math.atan2(-1.5, 1.5) #@TODO dont hardcode the targets x,y here
+    yaw = math.atan2(dy, dx) - starting_yaw_offset - current_yaw
 
     # we want the drone to maintain a close position to the target
     # note xymag cannot be negative because it is the sqrt of a positive
     # dividing by an arbitrary max dist normalizes (0,1)
     ideal_dist = 0.5
-    max_dist = 25
+    max_dist = 10
     dist_factor = abs(ideal_dist - xymag) / max_dist
 
     # a normalized (0, 1) value representing the amount of rotation needed
