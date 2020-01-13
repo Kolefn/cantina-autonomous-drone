ca0cfa0eeb9eddc1adc9596ce194c3bc430b95fe
Kole Nunley <kfnunley@gmail.com>
2020-01-13 13:32:06 -0500

    factor distance to target in reward function

--- deeprotor_ws/src/sagemaker_rl_agent/src/markov/rewards/default.py
+++ deeprotor_ws/src/sagemaker_rl_agent/src/markov/rewards/default.py
@@ -14,13 +14,25 @@ def reward_function(params):
     dy = target_y - y
     dz = target_z - z
 
+    xymag = math.sqrt(dx*dx + dy*dy)
+
     # rotations needed for drone camera to face target
-    pitch = math.atan2(dz, math.sqrt(dx*dx + dy*dy))
+    pitch = math.atan2(dz, xymag)
 
     starting_offset = math.atan2(-1.5, 1.5) #@TODO dont hardcode the targets x,y here
     yaw = math.atan2(dy, dx) - starting_offset
 
-    # higher reward for less needed rotation
-    reward = 1 - ((abs(pitch) + abs(yaw)) / (math.pi*2))
+    # we want the drone to maintain a close position to the target
+    # note xymag cannot be negative because it is the sqrt of a positive
+    # dividing by an arbitrary max dist normalizes (0,1)
+    ideal_dist = 0.5
+    max_dist = 25
+    dist_factor = abs(ideal_dist - xymag) / max_dist
+
+    # a normalized (0, 1) value representing the amount of rotation needed
+    rot_factor = ((abs(pitch) + abs(yaw)) / (math.pi*2))
+
+    # higher reward for factors closer to zero
+    reward = 1 - dist_factor - rot_factor
 
     return float(reward)
