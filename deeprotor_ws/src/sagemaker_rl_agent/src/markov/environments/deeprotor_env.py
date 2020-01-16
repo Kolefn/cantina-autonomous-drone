4fbcfe0662beb2c7e7968fe1c5a7a5f87f6e41e9
Kole Nunley <kfnunley@gmail.com>
2020-01-16 09:14:57 -0500

    queue size 1 to prevent action delays

--- deeprotor_ws/src/sagemaker_rl_agent/src/markov/environments/deeprotor_env.py
+++ deeprotor_ws/src/sagemaker_rl_agent/src/markov/environments/deeprotor_env.py
@@ -134,7 +134,7 @@ class DeepRotorEnv(gym.Env):
                                                        ResetDroneSrv)
                                         
 
-            self.velocity_pub = rospy.Publisher("/drone/command/motor_speed", Actuators, queue_size=3)
+            self.velocity_pub = rospy.Publisher("/drone/command/motor_speed", Actuators, queue_size=1)
 
             # Read in parameters
             self.job_type = rospy.get_param('JOB_TYPE')
